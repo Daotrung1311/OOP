3c4a97af216b09eeb3967f5930327bf599025f55
Daotrung1311 <trungdao131105@gmail.com>
2025-09-16 11:40:28 +0700

    init

---
+++ String_and_Array/AccessArray.java
@@ -0,0 +1,8 @@
+public class AccessArray {
+    public static void main(String[] args){
+        int arr[] = {12, 23, 23, 34};
+        for( int t : arr){
+            System.out.println(t);
+        }
+    }
+}
